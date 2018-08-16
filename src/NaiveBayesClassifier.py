619fd99119785d406db6cdde95ac45ccad4e73b5
Dan Adler <daadler0309@gmail.com>
2018-08-16 22:40:02 +1000

    initial push

---
+++ src/NaiveBayesClassifier.py
@@ -0,0 +1,14 @@
+"""
+NaiveBayesClassifier.py
+
+Written by Dan Adler
+Email: (daadler0309@gmail.com)
+GitHub: https://github.com/dadler6/
+
+Self-implementation of a Naive Bayes Classifier for text
+data.
+
+Package requirements:
+numpy
+pandas
+"""
---
+++ tests/test_NaiveBayesClassifier.py
@@ -0,0 +1,14 @@
+"""
+test_NaiveBayesClassifier.py
+
+Written by Dan Adler
+Email: (daadler0309@gmail.com)
+GitHub: https://github.com/dadler6/
+
+Test cases for NaiveBayesClassifier.py
+
+Package requirements:
+numpy
+pandas
+pytest
+"""
