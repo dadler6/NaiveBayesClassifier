f8a9bc09f18c16f4fe19cbe0ae84ae05bca78b21
Dan Adler <daadler0309@gmail.com>
2018-08-22 22:46:37 +1000

    first test works...more tomorrow

--- tests/test_NaiveBayesClassifier.py
+++ tests/test_NaiveBayesClassifier.py
@@ -22,7 +22,7 @@ import pandas as pd
 
 # Import specific module
 sys.path.insert(0, '../src')
-from NaiveBayesClassifier import NaiveBayesClassifier as nb
+from NaiveBayesClassifier import NaiveBayesClassifier as Nb
 
 
 @pytest.fixture(scope='module')
@@ -39,15 +39,40 @@ def set_format_input_params():
     test_data_series_y = pd.Series(data=test_data_list_y)
 
     # DataFrame
-    test_data_series_x_df = pd.Series({'X': test_data_list_x})
-    test_data_series_y_df = pd.Series({'Y': test_data_list_y})
+    test_data_series_x_df = pd.DataFrame({'X': test_data_list_x})
+    test_data_series_y_df = pd.DataFrame({'Y': test_data_list_y})
 
-    pass
+    return {
+        'list_x': test_data_list_x,
+        'list_y': test_data_list_y,
+
+        'series_x': test_data_series_x,
+        'series_y': test_data_series_y,
+
+        'df_x': test_data_series_x_df,
+        'df_y': test_data_series_y_df,
+    }
 
 
+# noinspection 801,PyShadowingNames
 def test_format_input_correct(set_format_input_params):
     """
     Test the nb.format_input function
+
+    :param set_format_input_params: dict<str:object>, the params to test
+        inputting different objects to program
     """
-    test_data_list = ['Hello, my name is Dan', 'Hello my name is Jill']
-    test_data_df = pd.DataFrame()
+    # Get inputs
+    test_data_list = set_format_input_params['list_x']
+    test_data_series = set_format_input_params['series_x']
+    test_data_df = set_format_input_params['df_x']
+
+    # Test
+    result_list = Nb.format_input(test_data_list, 'X')
+    result_series = Nb.format_input(test_data_series, 'X')
+    result_df = Nb.format_input(test_data_df, 'X')
+
+    # Assertions
+    assert test_data_list == result_list
+    assert test_data_list == result_series
+    assert test_data_list == result_df
